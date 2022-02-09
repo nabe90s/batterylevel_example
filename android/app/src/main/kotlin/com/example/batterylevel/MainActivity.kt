c201796553c3d8106c8fa1f0cb2461e3fc8e6a8b
Ryo WATANABE <30409123+nabe90s@users.noreply.github.com>
2022-02-09 13:53:38 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/batterylevel/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.batterylevel
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+
+
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "samples.flutter.dev/battery"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+
+            when (call.method) {
+                "getBatteryLevel" -> {
+                    val batteryLevel = getBatteryLevel()
+                    if (batteryLevel != -1) {
+                        result.success(batteryLevel)
+                    } else {
+                        result.error("UNAVAILABLE", "Battery level not available.", null)
+                    }
+                }
+                "getStringValueFromSharedPreferences" -> {
+                    val key: String? = call.argument<String>("key");
+                    val file: String? = call.argument<String>("file");
+                    when {
+                        key == null -> {
+                            result.error("KEY_MISSING", "Argument 'key' is not provided.", null)
+                        }
+                        file == null -> {
+                            result.error("FILE_MISSING", "Argument 'file' is not provided.", null)
+                        }
+                        else -> {
+                            val value: String? = getStringValue(file, key)
+                            result.success(value)
+                        }
+                    }
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel =
+                batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(
+                null,
+                IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            )
+            batteryLevel =
+                intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(
+                    BatteryManager.EXTRA_SCALE,
+                    -1
+                )
+        }
+
+        return batteryLevel
+    }
+
+    private fun getStringValueFromSharedPreferences(file: String, key: String): String? {
+        return context.getSharedPreferences(
+            file,
+            Context.MODE_PRIVATE
+        ).getString(key, null);
+    }
+
+}
+
